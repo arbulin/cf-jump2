163996139d3163ba642de64c8f3366a8b31e1cb5
arbulin <71180166+arbulin@users.noreply.github.com>
2024-11-19 11:05:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,11 +1,18 @@
 export default {
-  async fetch(request, env) {
-    let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
-      url.hostname = 'api-dev.laiguaba.com'
-      let new_request = new Request(url, request);
-      return fetch(new_request);
-    }
-    return env.ASSETS.fetch(request);
-  },
+    async fetch(request, env) {
+        let url = new URL(request.url);
+        if (url.pathname.startsWith('/')) {
+            const hostname = url.hostname;
+            if (hostname.includes('computer')){
+                url.hostname = env.api;
+            } else if (hostname.includes('index')){
+                url.hostname = env.h5;
+            } else if (hostname.includes('register')){
+                url.hostname = env.reg;
+            }
+            let new_request = new Request(url, request);
+            return fetch(new_request);
+        }
+        return env.ASSETS.fetch(request);
+    },
 };
