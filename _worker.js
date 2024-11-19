96091fe3f37e652315245c671bbc3d26d675c9fb
arbulin <71180166+arbulin@users.noreply.github.com>
2024-11-19 11:42:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,14 +3,15 @@ export default {
         let url = new URL(request.url);
         console.log(url);
         if (url.pathname.startsWith('/')) {
-            const hostname = url.hostname;
-            if (hostname.includes('computer')) {
-                url.hostname = env.API;
-            } else if (hostname.includes('index')) {
-                url.hostname = env.H5;
-            } else if (hostname.includes('register')) {
-                url.hostname = env.REG;
-            }
+            // const hostname = url.hostname;
+            // if (hostname.includes('computer')) {
+            //     url.hostname = env.API;
+            // } else if (hostname.includes('index')) {
+            //     url.hostname = env.H5;
+            // } else if (hostname.includes('register')) {
+            //     url.hostname = env.REG;
+            // }
+            url.hostname = 'api-prod.laiguaba.com';
             let new_request = new Request(url, request);
             let response = fetch(new_request);
             // 添加允许跨域访问的响应头
