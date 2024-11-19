6d97032b0846c6e91ac3bf7fa1038b1995c63602
arbulin <71180166+arbulin@users.noreply.github.com>
2024-11-19 11:37:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,15 +4,18 @@ export default {
         console.log(url);
         if (url.pathname.startsWith('/')) {
             const hostname = url.hostname;
-            if (hostname.includes('computer')){
+            if (hostname.includes('computer')) {
                 url.hostname = env.API;
-            } else if (hostname.includes('index')){
+            } else if (hostname.includes('index')) {
                 url.hostname = env.H5;
-            } else if (hostname.includes('register')){
+            } else if (hostname.includes('register')) {
                 url.hostname = env.REG;
             }
             let new_request = new Request(url, request);
-            return fetch(new_request);
+            let response = fetch(new_request);
+            // 添加允许跨域访问的响应头
+            response.headers.set('Access-Control-Allow-Origin', '*');
+            return response;
         }
         return env.ASSETS.fetch(request);
     },
