af4041a2b5a50369aa7406433d5eac4f84516da6
sun99413 <sun99413@sina.com>
2024-11-19 14:14:39 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,20 +3,19 @@ export default {
         let url = new URL(request.url);
         console.log(url);
         if (url.pathname.startsWith('/')) {
-            // const hostname = url.hostname;
-            // if (hostname.includes('computer')) {
-            //     url.hostname = env.API;
-            // } else if (hostname.includes('index')) {
-            //     url.hostname = env.H5;
-            // } else if (hostname.includes('register')) {
-            //     url.hostname = env.REG;
-            // }
-            url.hostname = 'zs-h5.laiguaba.com';
+            if (url.pathname.includes('computer')) {
+                url.hostname = env.API;
+            } else if (url.pathname.includes('index')) {
+                url.hostname = env.H5;
+            } else if (url.pathname.includes('register')) {
+                url.hostname = env.REG;
+            }
+            // url.hostname = 'api-dev.laiguaba.com';
             let new_request = new Request(url, request);
             return fetch(new_request);
             // 添加允许跨域访问的响应头
-            //response.headers.set('Access-Control-Allow-Origin', '*');
-            //return response;
+            // response.headers.set('Access-Control-Allow-Origin', '*');
+            // return response;
         }
         return env.ASSETS.fetch(request);
     },
