e6b1a1f826a64588b1132eaa0acd0e7f5cddda0b
sun99413 <sun99413@sina.com>
2024-11-21 18:03:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -28,17 +28,17 @@ export default {
 				url.hostname = 'oapi.dingtalk.com';
 				url.pathname = '/robot/send';
 				url.search = '?access_token=4cd4760ecf835953ec6e94084ea26e32a53f09711dce969419316577f1c58fb5';
-				//request.body = {
-				//			msgtype: "text",
-				//			text: {
-				//				content: msg,
-				//			},
-				//			isAtAll: true,
-				//		};
-				//request.method = 'POST';
-				//request.headers = {
-				//			"Content-Type": "application/json",
-				//		}
+				request.body = {
+							msgtype: "text",
+							text: {
+								content: msg,
+							},
+							isAtAll: true,
+						};
+				request.method = 'POST';
+				request.headers = {
+							"Content-Type": "application/json",
+						}
 				//https://oapi.dingtalk.com/robot/send?access_token=4cd4760ecf835953ec6e94084ea26e32a53f09711dce969419316577f1c58fb5
 				//let new_request1 = new Request('https://www.baidu.com', request);
 				//const str = await fetch(new_request1);
