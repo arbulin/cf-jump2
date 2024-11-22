165005dcc58fbaac539d1ea2cebebcf4d16469c2
sun99413 <sun99413@sina.com>
2024-11-22 09:31:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,6 @@
 export default {
 	async fetch(request, env) {
 		let url = new URL(request.url);
-		console.log(url);
 		if (url.pathname.startsWith("/")) {
 			if (url.pathname.includes("/computer/")) {
 				url.hostname = env.API;
@@ -22,29 +21,6 @@ export default {
 				url.hostname = env.OSS;
 			} else if (url.pathname.includes("/domain")) {
 				await sendMessage(request);
-				//url.hostname = 'www.baidu.com';
-				//url.pathname = '/index.html';
-				//let msg = `当前域名:${url.hostname},获取域名列表`;
-				//当有域名失败时请求新的域名列表
-				//url.hostname = 'oapi.dingtalk.com';
-				//url.pathname = '/robot/send';
-				//url.search = '?access_token=4cd4760ecf835953ec6e94084ea26e32a53f09711dce969419316577f1c58fb5';
-				//return new Response(`${request.method},${request.url},${request.body},${request.headers}`);
-				//request.body = {
-				//			msgtype: "text",
-				//			text: {
-				//				content: msg,
-				//			},
-				//			isAtAll: true,
-				//		};
-				//request.method = 'POST';
-				//request.headers = {
-				//			"Content-Type": "application/json",
-				//		}
-				//https://oapi.dingtalk.com/robot/send?access_token=4cd4760ecf835953ec6e94084ea26e32a53f09711dce969419316577f1c58fb5
-				//let new_request1 = new Request('https://www.baidu.com', request);
-				//const str = await fetch(new_request1);
-				//return new Response(`${JSON.stringify(str)}`);
 				return new Response(env.DOMAIN);
 			}
 			let new_request = new Request(url, request);
