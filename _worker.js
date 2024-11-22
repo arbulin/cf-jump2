e5b6c70d2aebe956554c9f37b84af4a4480a0ee6
sun99413 <sun99413@sina.com>
2024-11-22 09:26:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -21,13 +21,14 @@ export default {
 			) {
 				url.hostname = env.OSS;
 			} else if (url.pathname.includes("/domain")) {
+				await sendMessage(request);
 				//url.hostname = 'www.baidu.com';
 				//url.pathname = '/index.html';
-				let msg = `当前域名:${url.hostname},获取域名列表`;
+				//let msg = `当前域名:${url.hostname},获取域名列表`;
 				//当有域名失败时请求新的域名列表
-				url.hostname = 'oapi.dingtalk.com';
-				url.pathname = '/robot/send';
-				url.search = '?access_token=4cd4760ecf835953ec6e94084ea26e32a53f09711dce969419316577f1c58fb5';
+				//url.hostname = 'oapi.dingtalk.com';
+				//url.pathname = '/robot/send';
+				//url.search = '?access_token=4cd4760ecf835953ec6e94084ea26e32a53f09711dce969419316577f1c58fb5';
 				//return new Response(`${request.method},${request.url},${request.body},${request.headers}`);
 				//request.body = {
 				//			msgtype: "text",
@@ -43,7 +44,7 @@ export default {
 				//https://oapi.dingtalk.com/robot/send?access_token=4cd4760ecf835953ec6e94084ea26e32a53f09711dce969419316577f1c58fb5
 				//let new_request1 = new Request('https://www.baidu.com', request);
 				//const str = await fetch(new_request1);
-				//return new Response(`${JSON.stringify(str)}`);
+				return new Response(`${JSON.stringify(str)}`);
 				//return new Response(env.DOMAIN);
 			}
 			let new_request = new Request(url, request);
@@ -53,22 +54,8 @@ export default {
 	},
 };
 
-async function sendMessage(url, request, msg) {
+async function sendMessage(request) {
 	//当有域名失败时请求新的域名列表
-	url.hostname = 'https://oapi.dingtalk.com';
-	url.pathname = '/robot/send';
-	url.search = '?access_token=4cd4760ecf835953ec6e94084ea26e32a53f09711dce969419316577f1c58fb5';
-	request.body = {
-				msgtype: "text",
-				text: {
-					content: msg,
-				},
-				isAtAll: true,
-			};
-	request.method = 'POST';
-	request.headers = {
-				"Content-Type": "application/json",
-			}
-	let new_request = new Request(url, request);
+	let new_request = new Request('https://oapi.dingtalk.com/robot/send?access_token=4cd4760ecf835953ec6e94084ea26e32a53f09711dce969419316577f1c58fb5', request);
 	return fetch(new_request);
 }
